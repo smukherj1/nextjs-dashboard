d3025dca9fa262d34e1223f45fe1b6052363dbf5
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-04-08 20:38:53 -0400

    Fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const inter = Roboto({ weight: "400", subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
